7c7dde9de43b1398db0bdaf222e30f10e9635a7c
muxtorjonov2005 <muxtorjonoveldiyor@gmail.com>
2024-07-03 11:47:29 +0500

    first commit

---
+++ js/index.js
@@ -0,0 +1,83 @@
+// // 1-masala
+// function minFiveLetter(arr){
+//     return arr.filter(function(value){
+//         return value.length > 5
+//     }).map(value => value.charAt(0).toUpperCase() + value.slice(1))
+// }
+// console.log(minFiveLetter(['olma', 'anjirlar', 'behizor']));
+
+// 2-massala
+// function findSudent(arr){
+
+// }
+// const student =[
+//     {name: 'ali', age: 18},
+//     {name: 'vali', age:19},
+//     {name: 'gani', age:20}
+// ]
+// console.log(findSudent(student));
+
+// 3-massala
+
+// 4-masaala
+// function checkNumbers(arr){
+//     const positiv = arr.every(function(value){
+//         return value > 0
+//     })
+//     const hasBigNumber = arr.some(function(value){
+//         return value >= 100
+//     })
+//     if(positiv){
+//         console.log('Barcha sonlar katta');
+//     } else{
+//         console.log("manfiy son bor");
+//     }
+//     if(hasBigNumber){
+//         console.log('katta sonlar bor');
+//     }
+// }
+// console.log(checkNumbers([12, 5, 100]));
+
+// 5-masala
+// function creatNewMassiv(arr){
+//     let res = arr.map(function(value){
+//         return value ** 2
+//     })
+//     return res
+// }
+// console.log(creatNewMassiv([2,4,6]));
+
+// 6-masala
+
+// 7-masala
+// shu masalaga uncha tushunmadim bu
+
+// 1-masala
+// function toUpperCase(str){
+//     return str.toUpperCase()
+// }
+// console.log(toUpperCase('salom dunyo'));
+
+// 2-masala
+// function includes(str,word){
+//     return str.includes(word)
+// }
+// console.log(includes('salom bolalar', 'lar'));
+
+// 3-masala
+// function replace(str){
+//     return str.replace('JavaScript', 'JS')
+// }
+// console.log(replace('i love JavaScript'));
+
+// // 4-masala
+// function teskariQaytrish(str){
+//     return str.split('').reverse().join('')
+// }
+// console.log(teskariQaytrish('hello'));
+
+// 5-masal
+// function trim(str){
+//     return str.trim()
+// }
+// console.log(trim('  hello   '));
